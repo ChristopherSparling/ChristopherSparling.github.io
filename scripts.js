704b17bf3b75136aea7417ff6f90be5d6308f382
ChristopherSparling <christopher.j.sparling@gmail.com>
2018-09-22 19:39:30 -0400

    Flash on item select added

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-function show_div(id){   
-
-}
-
+// Toggles visibility of a selectable tag's children
 $(document).ready(function(){
     $(".ns").click(function(){
         $(this).next("div").toggle();
         $(this).toggleClass('s');
-        $(this).toggleClass('ns');
     });
+});
+
+$(document).ready(function(){
+    $(".s").click(function(){
+        $(this).next("div").hide(1000);
+        $(this).toggleClass('ns');
+        });
 });
 
 // Used for projects, updates arrows
@@ -16,5 +19,17 @@ $(document).ready(function(){
         $(this).prev(".arrow").text(function(i, text){
             return text === ">>" ? "<<" : ">>";
         });
+    });
+});
+
+$( function() {
+    $( ".ns, .s" ).click( function() {
+    
+        $(this).css("background-color", "#8a86857e")
+        $(this).animate({
+          backgroundColor: "#000"
+        }, 2000 );
+        $(this).css("background-color", "")
+      
     });
 });
