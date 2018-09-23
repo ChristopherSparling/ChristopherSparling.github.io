93739a4328751b7357863c28000e75278cf886f5
ChristopherSparling <christopher.j.sparling@gmail.com>
2018-09-23 11:24:13 -0400

    Highlight on click added

--- scripts.js
+++ scripts.js
@@ -1,35 +1,33 @@
 // Toggles visibility of a selectable tag's children
-$(document).ready(function(){
-    $(".ns").click(function(){
+$(document).ready(function () {
+    $(".ns").click(function () {
         $(this).next("div").toggle();
         $(this).toggleClass('s');
+        $(this).toggleClass('ns');
+
     });
 });
 
-$(document).ready(function(){
-    $(".s").click(function(){
-        $(this).next("div").hide(1000);
-        $(this).toggleClass('ns');
-        });
-});
-
 // Used for projects, updates arrows
-$(document).ready(function(){
-    $(".ns").click(function(){
-        $(this).prev(".arrow").text(function(i, text){
+$(document).ready(function () {
+    $(".ns").click(function () {
+        $(this).prev(".arrow").text(function (i, text) {
             return text === ">>" ? "<<" : ">>";
         });
     });
 });
 
-$( function() {
-    $( ".ns, .s" ).click( function() {
-    
-        $(this).css("background-color", "#8a86857e")
-        $(this).animate({
-          backgroundColor: "#000"
-        }, 2000 );
-        $(this).css("background-color", "")
-      
-    });
-});
+// Animate 
+$(document).ready(function () {
+            $(".ns, .s").click(function () {
+                    $(this).css("background-color", "white")
+                    $(this).animate({
+                            backgroundColor: "#000"
+                        }, {
+                            duration: 750,
+                            complete: function () {
+                                $(this).removeAttr('style')
+                            }
+                        });
+                    });
+            });
